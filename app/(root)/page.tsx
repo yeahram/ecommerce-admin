14abf1da3c3162aa1b43ad499e1eb689ea4d7538
yr623 <124855516+yeahram@users.noreply.github.com>
2023-09-08 09:41:58 +0900

    clerk 설정 완료

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
-      <Button>click me</Button>
+      <UserButton />
     </div>
   );
-}
+};
+export default SetupPage;
