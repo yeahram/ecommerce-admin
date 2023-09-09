38ead7bf87c564a405668fea19f8588a519f7624
yeahram <124855516+yeahram@users.noreply.github.com>
2023-09-09 15:22:19 +0900

    create prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
+import prismadb from "@/lib/prismadb";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const store = prismadb.store;
   return (
     <ClerkProvider>
       <html lang="en">
