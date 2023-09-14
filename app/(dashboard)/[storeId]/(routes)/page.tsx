a744e5ebf1737e2d1e503ebe4a0313a7813a1d9d
yeahram <124855516+yeahram@users.noreply.github.com>
2023-09-14 21:29:13 +0900

    dashboard setup

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,4 +1,15 @@
-const DashboardPage = () => {
-  return <div>This is Dashboard!</div>;
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Actice Store : {store?.name}</div>;
 };
 export default DashboardPage;
